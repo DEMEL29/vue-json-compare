a2f9949ce1d84f42104f0d0afbfa828d4e87fa8b
DEMEL29 <90153928+DEMEL29@users.noreply.github.com>
2025-09-05 23:59:52 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Сравнение JSON таблиц на Vue 3</h1>
+    <JsonCompare />
+  </div>
+</template>
+
+<script setup>
+import JsonCompare from "./components/JsonCompare.vue";
+</script>
